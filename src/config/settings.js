1578c0a77d1a4e9fb1e68a29070bb5708a7024d8
Peng_WIN <credencegp@gmail.com>
2022-08-14 23:39:48 +0200

    clean up

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
-	title: `All-In`,
+	title: `Peng's Blog`,
 	description: `Everything about life, technology and inbetween.`,
 	url: `https://www.peng-gong.ml`, // No trailing slash!
-	name: `All-In`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `P. Gong`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
 };
