f6fa488ab09c5186479bfcea209919ba3fe36471
Peng_WIN <credencegp@gmail.com>
2022-08-15 22:48:46 +0200

    set

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Peng's Blog`,
-	description: `Everything about life, technology and inbetween.`,
+	description: `Minimum your words, max your actions.`,
 	url: `https://www.peng-gong.ml`, // No trailing slash!
 	name: `Peng. G`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
